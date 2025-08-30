a04329bf0a9a51b8a33f57c548842270d8cf04a8
Nazario Di Cataldo <i3mnan@gmail.com>
2025-08-30 19:32:17 +0200

    Creazioni dei metodi per generare automaticamente le card e i risultati di ricerca

--- src/classes/meteo.ts
+++ src/classes/meteo.ts
@@ -14,9 +14,9 @@ export class Meteo {
     }
 
     //Richiedo le coordinate
-    async getCoordinates(limit: string = '5'): Promise<Coordinate[]> {
+    async getCoordinates(limit: number = 5): Promise<Coordinate[]> {
         try{
-            const res = await fetch(`${this.geoCodeEndpoint}?q=${this.citta}&limit=${limit}&appid=${this.apiKey}`);
+            //const res = await fetch(`${this.geoCodeEndpoint}?q=${this.citta}&limit=${limit}&appid=${this.apiKey}`);
             const data: Coordinate[] = await res.json();
             return data;
         } catch (err) {
@@ -28,7 +28,7 @@ export class Meteo {
 
     async getCondizione(lat: string, lon: string): Promise<Condizioni> {
         try {
-                const res = await fetch(`${this.weatherEndpoint}?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric&lang=it`);
+                //const res = await fetch(`${this.weatherEndpoint}?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric&lang=it`);
                 const data: Condizioni = await res.json();
                 return data;
 
@@ -49,4 +49,123 @@ export class Meteo {
         return `${this.weatherIconUrl}${id}@2x.png`;
     }
 
+    async generateList(wrapper: HTMLUListElement): Promise<void> {
+        //Prima svuoto il wrapper
+        wrapper.innerHTML = '';
+
+        //Mi prendo le località in base alla ricerca dell'utente
+        let localita: Coordinate[] = await this.getCoordinates(3);
+
+        //Se località è vuoto, e quindi non ci sono città da vedere, viene mostrato un messaggio di fallback
+            if(localita.length === 0) {
+                //Mi creo il tag li
+                let liTag = document.createElement('li');
+
+                //Aggiungo il li tag al wrapper
+                wrapper.append(liTag);
+
+                liTag.textContent = "Nessun risultato disponibile. Prova con un'altra ricerca";
+
+                liTag.classList = 'w-full p-4 text-center';
+
+                //Esco dalla funzione
+                return;
+            }
+
+
+        //Per ogni località mi prendo le condizioni e le metto nell'array
+        localita.forEach(async (e,pos) => {
+            //Per ogni elemento, mi creo il tag li e mi creo il layout
+            //Mi creo il tag li
+            let liTag = document.createElement('li');
+
+            //Aggiungo il li tag al wrapper
+            wrapper.append(liTag);
+
+           //Mi creo la singola condizione
+            let condizione: Condizioni = await this.getCondizione(e.lat, e.lon); 
+
+            //Mi creo l'oggetto date per la data
+            const dateToday: Date = new Date();
+
+            //Mi creo anche il tag a per il link alla pagina specifica
+            let aTag = document.createElement('a');
+            //Imposto il link dinamicamente
+            aTag.href = `meteo.html?lat=${localita[pos].lat}&lon=${localita[pos].lat}`;
+            //Inserisco il tag a dentro il tag li
+            liTag.append(aTag);
+            //Metto lo stile giusto per il tag a
+            aTag.classList = 'block w-full p-4 flex';
+
+            //Imposto il layout per ogni a
+            aTag.innerHTML = `
+                <figure class='flex flex-col items-start'>
+                    <img src=${this.getIconUrl(condizione.weather[0]?.icon)} alt="" class="w-[48px] h-auto">
+                    <figcaption class="text-center capitalize text-sm">${condizione.weather[0]?.description }</figcaption>
+                </figure>
+                <div class='localita flex text-primary items-end gap-1'>
+                    <strong class="text-xl text-medium city-name">${condizione.name}</strong>,
+                    <span class="country-name">${localita[pos].country}</span>
+                    <div class='flex gap-3 items-end'>
+                    <span class="time-zone">${dateToday.getUTCHours()} : ${dateToday.getUTCMinutes()}</span>
+                    <small class="time-zone">${dateToday.getUTCDate()} / ${dateToday.getUTCMonth()} / ${dateToday.getUTCFullYear()}</small>
+                </div>
+                </div>
+                <div class="flex justify-end meteo">
+                    <span class="degree text-2xl text-medium">${condizione.main?.temp} °</span>
+                </div>
+            `;
+        });
+    }
+
+    async generateCard(wrapper: HTMLDivElement): Promise<void> {
+        //Mi prendo le coordinate delle città
+        let localita: Coordinate[] = await this.getCoordinates(1);
+
+        //In base alle coordinate mi ricavo le informazioni sul meteo
+        let condizioni: Condizioni = await this.getCondizione( localita[0].lat, localita[0].lon);
+
+        //Mi creo l'oggetto date per la data
+        const dateToday: Date = new Date();
+
+        //Mi creo il tag a con il link dinamico
+        const aTag: HTMLAnchorElement = document.createElement('a');
+        //metto le classi ad a
+        aTag.classList = 'block w-full lg:w-1/3';
+        //Assegno il link dinamico alle card
+        aTag.href = `meteo.html?lat=${localita[0].lat}&lon=${localita[0].lat}`;
+
+        //Aggiungo il tag a al wrapper di card
+        wrapper.append(aTag);
+
+        //Mi creo dinamicamente la card
+        aTag.innerHTML = `
+            <!-- Card -->
+             <div class="w-full border-[1px] border-sky-500 p-5 rounded-lg bg-sky-500 flex flex-col items-center gap-4 cursor-pointer shadow-lg">
+              <figure class="weather-icon ">
+                <img src=${this.getIconUrl(condizioni?.weather[0]?.icon)} alt="" class="w-[96px] h-auto">
+                <figcaption class="text-center capitalize">${condizioni?.weather[0]?.description }</figcaption>
+              </figure>
+              <div class="localita flex text-primary items-end gap-1">
+                <h4 class="text-xl text-medium city-name">${condizioni.name}</h4>,
+                <span class="country-name">${localita[0].country}</span>
+                <div class='flex gap-3 items-end'>
+                    <span class="time-zone">${dateToday.getUTCHours()} : ${dateToday.getUTCMinutes()}</span>
+                    <small class="time-zone">${dateToday.getUTCDate()} / ${dateToday.getUTCMonth()} / ${dateToday.getUTCFullYear()}</small>
+                </div>
+              </div>
+              <div class="flex flex-col meteo">
+                <span class="degree text-2xl text-medium">${condizioni.main?.temp} °</span>
+              </div>
+              <div class="other w-full border-primary">
+                <ul class="list-conditions w-full flex flex-col items-center">
+                  <li class="wind">Vento: ${condizioni.wind?.speed} m/s</li>
+                  <li class="humidity">Umidità: ${condizioni.main?.humidity} %</li>
+                  <li class="pressure">Pressione atmosferica: ${condizioni.main?.pressure} h/Pa</li>
+                </ul>
+              </div>
+             </div>
+        `;
+        
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,45 @@
 //import './style.css';
 import { Meteo } from "./classes/meteo";
-import type { Condizioni } from "./interfaces/condizioni";
-import type { Coordinate } from "./interfaces/coordinate";
-
 
 const città: [string, string, string] = ['Milano', 'Roma', 'Napoli'];
 
 //Azioni che si svolgono al caricamento della pagina
 document.addEventListener('DOMContentLoaded', () => {
 
-  const cityTitles = document.querySelectorAll('.city-name');
-  const cityCountry = document.querySelectorAll('.country-name');
-  const cityDegree = document.querySelectorAll('.degree');
-  const cityIcons = document.querySelectorAll('.weather-icon > img');
-  const cityIconsDescription = document.querySelectorAll('.weather-icon > figcaption');
+  //Mi prendo il wrapper delle card
+  const cardWrapper: HTMLDivElement | null = document.querySelector('#card-wrapper');
+
+  //Mi prendo i vari input
+  const citySearch: NodeList = document.querySelectorAll('input[name="city"]')!;
+
+  //Mi prendo il dropdown, collegato all'input
+  const dropdownSearch: NodeList = document.querySelectorAll('.dropdown')!;
   
-  //Richiedo il meteo delle 3 città
-  città.forEach(async (e: string, pos: number) => {
-    const meteo = new Meteo(e);
-    //Mi prendo le coordinate delle città
-    let localita: Coordinate[] = await meteo.getCoordinates('1');
-    //In base alle coordinate mi ricavo le informazioni sul meteo
-    let condizioni: Condizioni = await meteo.getCondizione( localita[0].lat, localita[0].lon)
-    //Caso in cui le città non venissero trovate
-    if(localita.length === 0) {
-      //Fallback
-      cityTitles[pos].textContent = e;
-    }
 
-    //Mi configuro i nomi delle località
-    localita.forEach(e => {
-      cityTitles[pos].textContent = e.local_names.it;
-      cityCountry[pos].textContent = e.country;
-    })
 
-    //Configurazione delle condizioni
-    cityDegree[pos].textContent = `${condizioni.main?.temp} °`;
+  //Aggiungo tutti gli input al listener
+  citySearch.forEach((e, pos) => {
+    e.addEventListener('input', () => {
+    //Dropdown che scompare o compare in base a quello che scrive l'utente
+      if(e.value.length === 0) {
+        dropdownSearch[pos].classList.add('hidden');
+        dropdownSearch[pos].classList.remove('block');
+      } else {
+        dropdownSearch[pos].classList.remove('hidden');
+        dropdownSearch[pos].classList.add('block');
+      }
 
-    //Configurazione dell'icona
-    cityIcons[pos].src = meteo.getIconUrl(condizioni.weather[0].icon);
-    cityIconsDescription[pos].textContent = condizioni.weather[0].description;
-
+      //Mi creo un classe meteo con quello che l'utente scrive
+      const meteo: Meteo = new Meteo(e.value);
+      meteo.generateList(dropdownSearch[pos]);
+    });
   })
+  //Richiedo il meteo delle 3 città
+  if(cardWrapper) {
+    città.forEach(async (e: string) => {
+      const meteo = new Meteo(e);
+      await meteo.generateCard(cardWrapper);
+    });
+  }
+
 })
